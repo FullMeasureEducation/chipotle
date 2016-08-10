8ebde729456d96a4abbc541cd6a0fcb60e8d8067
fme-samuelbirk <sam@fullmeasureed.com>
2016-08-10 19:10:56 -0400

    easier email password config

--- chipotle.js
+++ chipotle.js
@@ -1,11 +1,13 @@
 module.exports = {
   'Demo test Google' : function (browser) {
+    var email = 'samuel.birk@gmail.com'
+    var password = 'password'
     browser
       .url('https://order.chipotle.com/Invitational/Index/122')
       .waitForElementVisible('body', 1000)
       .waitForElementVisible("input[name='Email']", 1000)
-      .setValue("input[name='Email']", 'ermail@gmail.com')
-      .setValue("input[name='Password']", 'somepassword')
+      .setValue("input[name='Email']", email)
+      .setValue("input[name='Password']", password)
       .click('#logonButton')
       .waitForElementVisible('#addNewEmail', 5000)
       .click('#NotifyAllSubmitted')
